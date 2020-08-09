e0ab76bd20955d1f0d2ac6f235c950c1b9d22d8b
Aishah Bushra <aishahbushra@outlook.com>
2020-08-09 17:37:39 +0100

    objects half done

--- src/objects.js
+++ src/objects.js
@@ -1,30 +1,44 @@
 const createPerson = (name, age) => {
-  // your code here
+   name: 'Frank'
+   age: 79
 };
 
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object.age;
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+  if(object[property]) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const isOver65 = person => {
-  // your code here
+if (person.age > 65) {
+  return true;
+} else {
+  return false;
+}
+
 };
 
 const getAges = people => {
-  // your code here
+    const mappedArr = people.map(function(person) {
+      return person.age;
+    });
+    return mappedArr;
 };
 
 const findByName = (name, people) => {
-  // your code here
+
 };
+
 
 const findHondas = cars => {
   // your code here
