b30c893eb34b26cabd50ae35f083f9caf52e6dbd
Aishah Bushra <aishahbushra@outlook.com>
2020-08-10 18:00:30 +0100

    some arrays done

--- src/arrays.js
+++ src/arrays.js
@@ -1,21 +1,31 @@
 const getNthElement = (index, array) => {
-  // your code here
+  const arrLength = array.length;
+  let positionToLookFor;
+  if (index + 1 > arrLength) {
+    positionToLookFor = index - arrLength;
+  } else {
+    positionToLookFor = index;
+  }
+  return array[positionToLookFor];
 };
 
 const arrayToCSVString = array => {
-  // your code here
+  return array.toString();
+
 };
 
 const csvStringToArray = string => {
-  // your code here
+  return string.split(',');
 };
 
 const addToArray = (element, array) => {
-  // your code here
+  array.push(element);
 };
 
 const addToArray2 = (element, array) => {
-  // your code here
+  const newArray = [...array]
+  newArray.push(element);
+  return newArray;
 };
 
 const removeNthElement = (index, array) => {
@@ -23,39 +33,55 @@ const removeNthElement = (index, array) => {
 };
 
 const numbersToStrings = numbers => {
-  // your code here
+return numbers.map(String);
 };
 
 const uppercaseWordsInArray = strings => {
-  // your code here
+  const mappedArr = strings.map(function(string) {
+    return string.toUpperCase();
+  });
+  return mappedArr;
 };
 
 const reverseWordsInArray = strings => {
-  // your code here
+
 };
 
+
 const onlyEven = numbers => {
-  // your code here
+  const evenNumbers = numbers.filter(function(number) {
+    return number % 2 == 0;
+  });
+  return evenNumbers;
 };
 
 const removeNthElement2 = (index, array) => {
-  // your code here
+  const newArray = [...array]
+  newArray.splice(2, 1);
+  return newArray;
 };
 
 const elementsStartingWithAVowel = strings => {
-  // your code here
+  const vowelWords = strings.filter(function(string) {
+    return (string.match('^[aeiouAEIOU].*'))
+  });
+  return vowelWords
 };
 
 const removeSpaces = string => {
-  // your code here
+  return string.replace(/\s/g, ""); // space remover from strings (regex)
 };
 
 const sumNumbers = numbers => {
-  // your code here
-};
+  // let array = [numbers];
+
+  array.reduce((accumulator, currentValue, currentIndex, array) => accumulator + currentValue);
+}; //
 
 const sortByLastLetter = strings => {
-  // your code here
+  let origArray =  ["Lannister", "Stark", "Greyjoy", "Targaryen"]
+  let splitArray = origArray.split("");
+
 };
 
 module.exports = {
